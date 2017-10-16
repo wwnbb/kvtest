292d145990250a9126c102e6a141789580eebffb
wwnbb <wwnbb1@gmail.com>
2017-10-16 08:33:10 +0300

    cleanup and fix js

--- kvtest/static/js/main.js
+++ kvtest/static/js/main.js
@@ -1,8 +1,15 @@
 $(function() {
     $('div.mytree div:has(div)').addClass('parent'); 
+
     $('div.active').children('div').toggle();
+
+    if ($('div.active').hasClass('parent')) {
+        $('div.active').addClass('expanded')
+    }
+
     $('div.active').parents('.parent').each(function() {
         $(this).children('div').toggle();
         $(this).addClass('expanded');
     });
+
 }());
